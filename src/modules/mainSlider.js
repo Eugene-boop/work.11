944b768ef560058accb7a90caccdde19be39c19a
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-03-17 22:30:35 +0600

    мультислайдер

--- src/modules/mainSlider.js
+++ src/modules/mainSlider.js
@@ -1,20 +1,28 @@
-const mainSlider = () => {
+const mainSlider = ({ slideName, sliderName, dotUlName = none, dotClass, dotActiveClass, arrowName, arrowNextName, arrowPrevName, }) => {
   const slide = document.querySelectorAll('.main-slider .slide'),
     dotUl = document.querySelector('.dots'),
     slider = document.querySelector('.main-slider');
+    let dot;
 
-  // const addDots = () => {
-  //   let dotStr = '';
-  //   for (let i = 0; i < slide.length; i++) {
-  //     dotStr += '<li class="dot"></li>';
-  //   }
-  //   dotUl.insertAdjacentHTML("afterbegin", dotStr);
-  //   dotUl.childNodes[0].classList.add('dot-active');
-  // };
+  if (dotUlName) {
+    const addDots = () => {
+      let dotStr = '';
+      for (let i = 0; i < slide.length; i++) {
+        dotStr += `<li class="${dotClass}"></li>`;
+      }
+      dotUl.insertAdjacentHTML("afterbegin", dotStr);
+      dotUl.childNodes[0].classList.add(dotActiveClass);
+    };
 
-  // addDots();
+    addDots();
 
-  // const dot = dotUl.childNodes;
+    dot = dotUl.childNodes;
+  }
+    
+  if (arrowName) {
+    slider.insertAdjacentHTML('beforeend',`<a href="#" class="${arrowName} ${arrowLeftName}"></a>
+    <a href="#" class="${arrowName} ${arrowPrevName}"></a>`);
+  }
 
   let currentSlide = 0,
     interval;
@@ -37,11 +45,15 @@ const mainSlider = () => {
 
   const autoPlaySlide = () => {
     prevSlide(slide, currentSlide);
-    // prevSlide(dot, currentSlide, 'dot-active');
+    if (dotUlName) {
+      prevSlide(dot, currentSlide, dotActiveClass);
+    } 
     currentSlide++;
     if (currentSlide >= slide.length) currentSlide = 0;
     nextSlide(slide, currentSlide);
-    // nextSlide(dot, currentSlide, 'dot-active');
+    if (dotUlName) {
+      nextSlide(dot, currentSlide, dotActiveClass);
+    } 
   };
 
   const startSlide = (time = 3000) => {
@@ -54,42 +66,49 @@ const mainSlider = () => {
 
   startSlide(4000);
   // не работает из-за того, что часть кнопок перекрывает изображение справа
-  // slider.addEventListener('click', event => {
-    
-
-  //   const target = event.target;
-  //   console.log('target: ', target);
-
-  //   if (!target.matches('.dot')) return;
-
-  //   prevSlide(slide, currentSlide);
-  //   prevSlide(dot, currentSlide, 'dot-active');
-
-  //   dot.forEach((elem, ind) => {
-  //     if (elem === target) {
-
-  //       currentSlide = ind;
-  //     }
-  //   });
-    
-
-  //   nextSlide(slide, currentSlide);
-  //   nextSlide(dot, currentSlide, 'dot-active');
-  // });
-
-  // slider.addEventListener('mouseover', e => {
-  //   if (e.target.matches('.portfolio-btn')  ||
-  //   event.target.matches('.dot')) {
-  //     stopSlide();
-  //   }
-  // });
-
-  // slider.addEventListener('mouseout', e => {
-  //   if (e.target.matches('.portfolio-btn')  ||
-  //   event.target.matches('.dot')) {
-  //     startSlide();
-  //   }
-  // });
+  if (dotUlName || arrowName) {
+    slider.addEventListener('click', event => {
+      event.preventDefault();
+  
+      const target = event.target;
+  
+      if (!target.matches('#arrow-right, #arrow-left, .dot')) return;
+  
+      prevSlide(slide, currentSlide);
+      prevSlide(dot, currentSlide, 'dot-active');
+  
+      if (target.matches('#arrow-right')) {
+        currentSlide++;
+      } else if (target.matches('#arrow-left')) {
+        currentSlide--;
+      } else if (target.matches('.dot')) {
+        dot.forEach((elem, ind) => {
+          if (elem === target) {
+            currentSlide = ind;
+          }
+        });
+      }
+  
+      if (currentSlide >= slide.length) currentSlide = 0;
+      if (currentSlide < 0) currentSlide = slide.length - 1;
+      nextSlide(slide, currentSlide, 'portfolio-item-active');
+      nextSlide(dot, currentSlide, 'dot-active');
+    });
+  
+    slider.addEventListener('mouseover', e => {
+      if (e.target.matches('.portfolio-btn')  ||
+      event.target.matches('.dot')) {
+        stopSlide();
+      }
+    });
+  
+    slider.addEventListener('mouseout', e => {
+      if (e.target.matches('.portfolio-btn')  ||
+      event.target.matches('.dot')) {
+        startSlide();
+      }
+    });
+  }
 };
 
 export default mainSlider;  
